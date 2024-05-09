26452ef4587dd1d5f8df45a66a97da3aad4797e0
Nicolas-jnn <njenni@ikmail.com>
2024-05-09 12:48:34 +0200

    change path st_main

--- st_main.py
+++ st_main.py
@@ -1,21 +1,41 @@
 import streamlit as st
+from pathlib import Path
+
+#path image for mac and windows
+path = Path(__file__).parent
+path = str(path) + '/images/dist_img.png'
+
+
 
 
 col1, col2 = st.columns(2)
 
+app_path = 'http://localhost:8501'
+page_file_path = 'pages/st_Distillation.py'
+page1 = page_file_path.split('/')[1][0:-3]  # get "page1"
+
+
 with col1:
-    st.image('./images/dist_img.png', use_column_width=True)
+    st.image(path, use_column_width=True)
 
 
-    st.page_link("pages/st_Distillation.py", label="Binary distillation")
-    #center page link
-    st.markdown("<h1 style='text-align: center;'>Distillation</h1>", unsafe_allow_html=True)
-    st.markdown("<h3 style='text-align: center;'>Binary distillation</h3>", unsafe_allow_html=True)
+    st.markdown(f'''<h3 style='text-align: center;'><a href="{app_path}/{page1}" target="_self">Distillation</a></h3>''',
+    unsafe_allow_html=True
+)
+    #remove the blue color and underline of the link
+    st.markdown(
+        '''
+        <style>
+        a {
+            text-decoration: none;
+            color: red;
+        }
+        </style>
+        ''', unsafe_allow_html=True
+    )
     st.markdown("<p style='text-align: center;'>This app calculates the number of distillation states needed for a binary distillation column.</p>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center;'>The McCabe-Thiele diagram is used to determine the number of distillation states needed for a binary distillation column.</p>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center;'>The inputs are the following:</p>", unsafe_allow_html=True)
-    #link to the Distillation.py file
-    st.markdown("<p style='text-align: center;'>F: Feed flow rate</p>", unsafe_allow_html=True)
+    
+
 
     
 
