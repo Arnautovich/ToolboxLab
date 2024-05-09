ee67bdd2e01f735ef8851420e8c7c8f3018f6cc9
BrunoGonOli <bru.oliveira2502@gmail.com>
2024-05-09 17:59:51 +0200

    update streamlit bruno

--- Home_Page.py
+++ Home_Page.py
@@ -7,13 +7,11 @@ path_img1 = str(path) + '/images/dist_img.png'
 path_img2 = str(path) + '/images/chem_img.png'
 
 
-
-
 col1, col2 = st.columns(2)
 
 app_path = 'http://localhost:8501'
-page_file_path1 = 'pages/st_Distillation.py'
-page_file_path = 'pages/st_Chemical_rxn.py'
+page_file_path1 = 'pages/Distillation.py'
+page_file_path = 'pages/Chemical_Reaction.py'
 page1 = page_file_path1.split('/')[1][0:-3]  # get "page1"
 page2 = page_file_path.split('/')[1][0:-3]  # get "page2"
 
@@ -62,3 +60,4 @@ with col2:
 
     #Stoitometry of chemical reaction and properties of the reactants
     st.markdown("<p style='text-align: center;'>This app calculates the stoichiometry of a chemical reaction and the properties of the reactants/products.</p>", unsafe_allow_html=True)
+    
