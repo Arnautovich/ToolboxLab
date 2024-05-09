9ee26209766f0ee902a22ba158a9cdb4e68bd6c7
BrunoGonOli <bru.oliveira2502@gmail.com>
2024-05-09 15:06:51 +0200

    update streamlit bruno

--- pages/st_Chemical_rxn.py
+++ pages/st_Chemical_rxn.py
@@ -1,15 +1,24 @@
 import streamlit as st
 from chemical_rxn import equation
 
+col1, col2 = st.columns(2)
+
+with col1:
+    tog1 = st.toggle("View image of reaction")
+
+with col2:
+    tog2 = st.toggle("View products properties", help="")
+
 reac = st.text_input("Please enter your reaction as follow [list of reactants] -> [list of products] : ", 
-                     help="If your text doesn't work try different names for your compounds")
+                     help="If your text doesn't work try different names for your compounds",
+                     placeholder="NaOH + HCl -> H2O + NaCl")
 
-rxn = equation(reac)
+if reac != "":
+    rxn = equation(reac)
+    if tog1:
+        st.image(rxn.draw_reaction())
 
-st.image(rxn.draw_reaction())
-
-tog1 = st.toggle("View products properties", help="")
-if tog1:
-    st.dataframe(rxn.get_reaction_properties())
+    if tog2:
+        st.dataframe(rxn.get_reaction_properties())
 
 # description du tableau + input par default dans box + modifier text input
