0c4543f514abcc119e2e73d415ee738514e6b996
BrunoGonOli <bru.oliveira2502@gmail.com>
2024-05-11 12:38:25 +0200

    modif st chemical rxn + modif pH

--- pH.py
+++ pH.py
@@ -2,33 +2,72 @@ import numpy as np
 
 def pHfromconc (ca:float=1, cb:float=1, pka:float=None, Ka:float=None, Kb:float=None) -> float:
     '''
-    
+    This function calculates the pH of a weak acid/base solution knowing the concentrations of compounds and either the pKa, Ka or Kb
+
+    input : 
+    ca: The concentration of acid [AH]
+    cb: The concentration of the conjugate base [A-]
+    pka: The pKa of the couple
+    *
+    Ka: The acidity constant of the couple
+    Kb: The basicity constant of the couple 
+
+    output:
+    pH as float
     '''
+
     if pka == None and Ka != None:
-        pka = -np.log10(K)
+        pka = -np.log10(Ka)
     else:
         pka = -np.log10(1*10**(-14)/Kb)
 
-    return pka + np.log10(cb/ca)
+    return float(pka + np.log10(cb/ca))
+
 
 def concfrompH (pH:float=7, ca:float=None, cb:float=None, pka:float=None, Ka:float=None, Kb:float=None) -> float:
     '''
-    Define one concentration to calculate the second
+    This function calculates the concentration of a weak acid/base knowing the pH and the concentration of its conjugate
+
+    input:
+    pH
+    ca: The concentration of acid [HA]
+    cb: The concentration of base [A-]
+    pka: The pKa of the couple
+    *
+    Ka: The acidity constant of the couple
+    Kb: The basicity constant of the couple
+
+    output:
+    The concentration missing as float
     '''
 
     if pka == None and Ka != None:
-        pka = -np.log10(K)
+        pka = -np.log10(Ka)
     else:
         pka = -np.log10(1*10**(-14)/Kb)
 
     if ca == None:
         ca = cb/(10**(pH-pka))
-        return ca
+        return float(ca)
     else:
         cb = ca*10**(pH-pka)
-        return cb
+        return float(cb)
+
+def strongacid (ca:float) -> float:
+    '''
+    This function calculates the pH of a strong acid in water
+    '''
+    return -np.log10(0.5*ca + np.sqrt(0.25*ca**2 + 10**(-14)))
+
+def strongbase (cb:float) -> float:
+    '''
+    
+    '''
+    return -np.log10(-0.5*cb + np.sqrt(0.25*cb**2 + 10**(-14)))
 
 
-
+# changer deux première fonction par buffer 
 print("le pH est : ", pHfromconc(0.4, 1, Kb=1.8*10**(-5)))
 print("la concentration est :", concfrompH(9.65, 0.4, None, Kb=1.8*10**(-5)))
+print(strongacid(0.234))
+print(strongbase(2*0.013))
--- pages/Chemical_Reaction.py
+++ pages/Chemical_Reaction.py
@@ -4,20 +4,23 @@ from chemical_rxn import equation
 col1, col2 = st.columns(2)
 
 with col1:
-    tog1 = st.toggle("View image of reaction")
+    tog1 = st.toggle("View image of reaction", value=True)
 
 with col2:
-    tog2 = st.toggle("View products properties")
+    tog2 = st.toggle("View products properties", value=True)
 
-reac = st.text_input("Please enter your reaction as follow [list of reactants] -> [list of products] : ", 
+reac = st.text_input('''Please enter your reaction as follow:  
+                     reactant1 + reactant2 + ... -> product1 + product2 + ...''', 
                      help="If your text doesn't work try different names for your compounds",
-                     placeholder="NaOH + HCl -> H2O + NaCl")
+                     placeholder="NaOH + HCl -> H2O + NaCl   or   methane + O2 -> water + CO2")
 
 if reac != "":
     rxn = equation(reac)
     if tog1:
         st.image(rxn.draw_reaction())
 
+    st.write('''In this data frame:  
+             Stoichiometric coefficients are negative for reactants and positive for products''')
     if tog2:
         st.dataframe(rxn.get_reaction_properties())
 
