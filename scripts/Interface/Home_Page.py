cef799bd4efc7843dfa9a86da66023570b2e3623
BrunoGonOli <bru.oliveira2502@gmail.com>
2024-05-20 16:50:12 +0200

    update

--- scripts/Interface/Home_Page.py
+++ scripts/Interface/Home_Page.py
@@ -15,12 +15,14 @@ page_file_path2 = 'pages/Chemical_Reaction.py'
 page_file_path3 = 'pages/pH_Calculator.py'
 page_file_path4 = 'pages/Liquid-Liquid_Extraction.py'
 page_file_path5 = 'pages/Boiling_Point_at_new_Pressure.py'
+page_file_path6 = 'pages/Usefull_Functions.py'
 
 page1 = page_file_path1.split('/')[1][0:-3]  # get "page1"
 page2 = page_file_path2.split('/')[1][0:-3]  # get "page2"
 page3 = page_file_path3.split('/')[1][0:-3]
 page4 = page_file_path4.split('/')[1][0:-3]
 page5 = page_file_path5.split('/')[1][0:-3]
+page6 = page_file_path6.split('/')[1][0:-3]
 
 with col1:
     st.image(path_img1, use_column_width=True)
@@ -37,8 +39,7 @@ with col1:
             color: red;
         }
         </style>
-        ''', unsafe_allow_html=True
-    )
+        ''', unsafe_allow_html=True)
     st.markdown("<p style='text-align: center;'>This app calculates the number of distillation states needed for a binary distillation column.<br />&nbsp;</p>", unsafe_allow_html=True) # <br />&nbsp;
     
 
@@ -93,8 +94,7 @@ with col2:
             color: red;
         }
         </style>
-        ''', unsafe_allow_html=True
-    )
+        ''', unsafe_allow_html=True)
 
     #Stoitometry of chemical reaction and properties of the reactants
     st.markdown("<p style='text-align: center;'>This app calculates the stoichiometry of a chemical reaction and the properties of the reactants/products.</p>", unsafe_allow_html=True)
@@ -103,7 +103,7 @@ with col2:
 
     st.markdown(f'''<h3 style='text-align: center;'><a href="{app_path}/{page4}" target="_self">Liquid-Liquid Extraction</a></h3>''',
     unsafe_allow_html=True)
-    #remove the blue color and underline of the link
+
     st.markdown(
         '''
         <style>
@@ -112,7 +112,24 @@ with col2:
             color: red;
         }
         </style>
-        ''', unsafe_allow_html=True
-    )
+        ''', unsafe_allow_html=True)
+    
     st.markdown("description", unsafe_allow_html=True)
 
+    st.image(path_img3, use_column_width=True)
+
+    st.markdown(f'''<h3 style='text-align: center;'><a href="{app_path}/{page6}" target="_self">Usefull Functions</a></h3>''',
+    unsafe_allow_html=True)
+
+    st.markdown(
+        '''
+        <style>
+        a {
+            text-decoration: none;
+            color: red;
+        }
+        </style>
+        ''', unsafe_allow_html=True)
+    
+    st.markdown("description", unsafe_allow_html=True)
+    
