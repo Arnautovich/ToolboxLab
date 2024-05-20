fe561883beae02aa7f0b3e43a7a4b1685029d28c
Arnautovich <arnaucasalprimperez@epfl.ch>
2024-05-20 14:52:35 +0200

    nice

--- src/Toolbox/Pressure_TB.py
+++ src/Toolbox/Pressure_TB.py
@@ -1,16 +1,28 @@
+
+import requests
 import numpy as np
-
-
-def clausius_clapeyron (T,P): 
+def resolve_name_to_cas(molecule_name):
     
-    return 1/((1/T)-(np.log(P/1.01325)*R/(T*(4.4+np.log(T)))))
+        # Construct the URL for the CIR service
+    url = f"https://commonchemistry.cas.org/results?q={molecule_name}"
+
+        # Send a GET request to the CIR service
+    response = requests.get(url)
+
+    find = "Result: "
+    start=response.text.find(find)+len(find)
+    end = response.text.find("," ,start)
+    return response.text[start: end]
+
+    
+
 
 
 import chemicals 
-def clausius_clapeyron_enthalpy (Pressure,CASRN):
+def clausius_clapeyron_enthalpy (Pressure,STR):
 #P in bar
 # T in K
-    
+    CASRN = resolve_name_to_cas(STR)
    
     Liquid_heat_compound = chemicals.reaction.Hfl(CASRN, method=None)
     
@@ -21,3 +33,5 @@ def clausius_clapeyron_enthalpy (Pressure,CASRN):
     latent_heat_compound = Gas_heat_compound - Liquid_heat_compound
     
     New_Boiling_Point_at_P = 1/((1/T)-(np.log(Pressure/1.01325)*8.314/(latent_heat_compound)))
+
+    return New_Boiling_Point_at_P
