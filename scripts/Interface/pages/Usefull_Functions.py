d01091f6cc91b60b5a32e674084ce5fbd553b490
BrunoGonOli <bru.oliveira2502@gmail.com>
2024-05-21 15:01:06 +0200

    update streamlit

--- scripts/Interface/pages/Usefull_Functions.py
+++ scripts/Interface/pages/Usefull_Functions.py
@@ -2,10 +2,18 @@ import streamlit as st
 import Toolbox.chemical_rxn as tc
 import Toolbox.Pressure_TB as tp
 
-st.header("Converte molecule name to SMILES")
+st.header("Usefull Functions")
 
-name = st.text_input("The name of your molecule:")
+st.subheader("Converte molecule name to SMILES")
+
+name = st.text_input("The name of your molecule:", value="Water", help="If the name used does not work, try different names for your compound")
 el = tc.elements(name)
 
 smile = el.smile
 prop = el.properties
+nbre = el.atoms_nb
+
+st.write(f'''The smile of {name} is: {smile}  
+The atom list is : {nbre}  
+usefull properties are :''')
+st.dataframe(prop, width=700)
